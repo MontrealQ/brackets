b71a32491eb30e434d8ad1bfd5a3b3ef9ebae294
Artsiom Bandaryk <a.bondarik13@gmail.com>
2021-03-15 23:00:49 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const brackets = bracketsConfig.map((arrElement) => arrElement.join(""));
+    let isUpdated = true;
+    while (isUpdated) {
+        isUpdated = false;
+        brackets.forEach((arrElement) => {
+            const newStr = str.replace(arrElement, "");
+            if (newStr !== str) {
+                str = newStr;
+                isUpdated = true;
+            }
+        });
+    }
+    return !str;
+};
